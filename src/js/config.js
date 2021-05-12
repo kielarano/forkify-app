eb7dc44507f04047eda49a385db45dd40fa3808c
kielarano <arano.ezekiel@gmail.com>
2021-05-12 20:09:05 +0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'adc363fe-d598-4400-b45e-18d810c3d06b';
+export const MODAL_CLOSE_SEC = 2.5;
